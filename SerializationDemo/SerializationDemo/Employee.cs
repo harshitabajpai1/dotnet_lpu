f3356020606574a2555fbd2933363c8fb731ee1c
Harshita Bajpai <harshitabajpai2288@gmail.com>
2026-01-10 11:55:38 +0530

    jan 10 file handling and Serialization

---
+++ SerializationDemo/SerializationDemo/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationDemo
+{
+    internal class Employee
+    {
+    }
+}
