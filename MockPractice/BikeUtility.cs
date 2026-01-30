ac8f8a3642bde965ce6d77db1257e655514443ae
Harshita Bajpai <harshitabajpai2288@gmail.com>
2026-01-30 23:45:58 +0530

    GOAIR custom exception

---
+++ MockPractice/BikeUtility.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+class BikeUtility
+{
+    public void AddBikeDetails(string model, string brand, int pricePerDay)
+    {
+        int key = Program.bikeDetails.Count+1;
+        //dictionary it defined in program class
+        Bike bike = new Bike
+        {
+            Model = model,
+            Brand = brand,
+            PricePerDay = pricePerDay
+        };
+        Program.bikeDetails.Add(key,bike);
+    }
+    public SortedDictionary<string,List<Bike>> GroupByBrand()
+    {
+        SortedDictionary<string,List<Bike>> BikeGroupedbyBrand =
+        new SortedDictionary<String,List<Bike>>();
+
+        foreach (var item in Program.bikeDetails.Values)
+        {
+            if (!BikeGroupedbyBrand.ContainsKey(item.Brand))
+            {
+                BikeGroupedbyBrand[item.Brand] = new List<Bike>();
+            }
+
+            BikeGroupedbyBrand[item.Brand].Add(item);
+        }
+        return BikeGroupedbyBrand;
+    }
+}
