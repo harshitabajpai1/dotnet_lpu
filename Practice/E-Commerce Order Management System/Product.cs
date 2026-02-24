51bd7e9cbf46e4ce6332c0012cd9c83a7adf51c2
Harshita Bajpai <harshitabajpai2288@gmail.com>
2026-02-24 16:11:03 +0530

    MVC core in asp.net

---
+++ Practice/E-Commerce Order Management System/Product.cs
@@ -0,0 +1,12 @@
+class Product
+{
+    public int Id{get;set;}
+    public string Name{get; set;}
+    public decimal price{get;set;}
+    public int Stock{get;set;}
+
+    public Product()
+    {
+        
+    }
+}
---
+++ Practice/Fitness App/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Collections.Generic;
+class MemberStats
+{
+    public string Name{get;set;}
+    public List<int> WeeklyStepCount{get;set;}
+}
+class Program
+{
+    static List<MemberStats> LeaderBoard = new List<MemberStats>();
+    public static void AddMember(MemberStats member)
+    {
+        LeaderBoard.Add(member);
+    }
+
+    public static Dictionary<string,int> MoreThanThreshold(int StepThreshold)
+    {
+        Dictionary<string,int> result = new Dictionary<string,int>();
+
+        return result;
+    }
+}
