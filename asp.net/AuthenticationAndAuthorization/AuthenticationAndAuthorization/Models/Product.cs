188aa437129506f1ae0a222b38ef13592d80ba79
Harshita Bajpai <harshitabajpai2288@gmail.com>
2026-02-26 22:04:50 +0530

    Authentication and authorization in asp.net

---
+++ asp.net/AuthenticationAndAuthorization/AuthenticationAndAuthorization/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthenticationAndAuthorization.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public int StockQuantity {  get; set; }
+
+
+} 
+}
---
+++ asp.net/AuthenticationAndAuthorization/AuthenticationAndAuthorization/Models/ProductRepo.cs
@@ -0,0 +1,23 @@
+﻿namespace AuthenticationAndAuthorization.Models
+{
+    public class ProductRepo
+    {
+        public static List<Product> cosmeticProducts = null;
+        public ProductRepo()
+        {
+            cosmeticProducts = new List<Product>
+            {
+                new Product{ProductId=1, Name="Lipstick", Cost=19.99M, Description="A long-lasting lipstick that provides vibrant color and a smooth finish.", Category="Makeup", Brand="Luxe Beauty", StockQuantity=100},
+                new Product{ProductId=2, Name="Foundation", Cost=29.99M, Description="A lightweight foundation that offers buildable coverage and a natural finish.", Category="Makeup", Brand="Glow Cosmetics", StockQuantity=150},
+                new Product{ProductId=3, Name="Mascara", Cost=14.99M, Description="A volumizing mascara that lengthens and defines lashes for a dramatic look.", Category="Makeup", Brand="Luxe Beauty", StockQuantity=200},
+                new Product{ProductId=4, Name="Blush", Cost=24.99M, Description="A silky blush that adds a natural flush of color to the cheeks.", Category="Makeup", Brand="Glow Cosmetics", StockQuantity=120},
+                new Product{ProductId=5, Name="Eyeshadow Palette", Cost=39.99M, Description="A versatile eyeshadow palette with a range of shades for creating endless eye looks.", Category="Makeup", Brand="Luxe Beauty", StockQuantity=80},
+                new Product{ProductId=6, Name="Face Cream", Cost=49.99M, Description="A nourishing face cream that hydrates and revitalizes the skin.", Category="Skincare", Brand="Glow Cosmetics", StockQuantity=90},
+            };
+        }
+        public List<Product> GetAllCosmeticProducts()
+        {
+            return cosmeticProducts; 
+        }
+    }
+}
